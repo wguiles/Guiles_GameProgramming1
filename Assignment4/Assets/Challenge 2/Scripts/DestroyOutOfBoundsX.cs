8531b47556ce80af21d0d12e537a2ad3fe2cae67
Warren <guileswa@gmail.com>
2019-09-20 18:11:24 -0500

    Revert "Revert "Challenge 2""
    This reverts commit 9e6e10ef8740eb029a157c43efef97df907425ce.

---
+++ Assignment4/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    /*
+    Warren Guiles
+    CIS_456_Assignment_4
+
+    This script makes sure that objects out of the player's view 
+    and/or objects out of bounds are destroyed 
+    as to protect performance
+
+     */
+public class DestroyOutOfBoundsX : MonoBehaviour
+{
+    private float leftLimit = -40;
+    private float bottomLimit = -5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Destroy dogs if x position less than left limit
+        if (transform.position.x < leftLimit)
+        {
+            Destroy(gameObject);
+        } 
+        // Destroy balls if y position is less than bottomLimit
+        else if (transform.position.y < bottomLimit)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Assignment4/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnRandomBallCoroutine());
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        //spawn a random ball based on a random number
+        int randomNum = Random.Range(0, 3);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[randomNum], spawnPos, ballPrefabs[randomNum].transform.rotation);
+
+    }
+
+    IEnumerator SpawnRandomBallCoroutine()
+    {
+        while(true)
+        {
+            int RandomWaitTime = Random.Range(3,6);
+            yield return new WaitForSeconds(RandomWaitTime);
+            SpawnRandomBall();
+        }
+    }
+
+}
