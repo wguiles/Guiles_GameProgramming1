5eb1e15d759e212b691737aeb60c1853bbb41ccf
Warren <guileswa@gmail.com>
2019-10-03 18:22:36 -0500

    Assignment 6
    Challenge 4 added

---
+++ Assignment6/Assets/Challenge 4/Scripts/RotateCameraX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCameraX : MonoBehaviour
+{
+        /*
+    Warren Guiles
+    RotateCameraX
+    Assignment 6
+
+    This script rotates the camera based on the arrow keys pressed
+
+     */
+    private float speed = 200;
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * speed * Time.deltaTime);
+
+        transform.position = player.transform.position; // Move focal point with player
+
+    }
+}
